166e09e8e0d5feddab25ac2b83bc03913dd012f3
Brais Fernandez <braisoncrece@gmail.com>
2022-07-21 23:43:10 +0200

    Ejercicios resueltos

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.setEdad(25);
+        persona1.setNombre("Manolo");
+        persona1.setTelefono(687052011);
+
+        System.out.println(persona1);
+
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+        System.out.println("Telefono: " + persona1.getTelefono());
+    }
 }
